13ad7782428bb65b670386d6fcaf255cf74ebdce
Dimitar Pashov <d.pashov@gmail.com>
2015-11-14 22:52:04 +0000

    py3.5 fixes

--- psshlib/manager.py
+++ psshlib/manager.py
@@ -6,6 +6,7 @@ import select
 import signal
 import sys
 import threading
+import fcntl
 
 try:
     import queue
@@ -209,6 +210,7 @@ class IOMap(object):
 
         # Setup the wakeup file descriptor to avoid hanging on lost signals.
         wakeup_readfd, wakeup_writefd = os.pipe()
+        fcntl.fcntl(wakeup_writefd, fcntl.F_SETFL, os.O_NONBLOCK)
         self.register_read(wakeup_readfd, self.wakeup_handler)
         # TODO: remove test when we stop supporting Python <2.5
         if hasattr(signal, 'set_wakeup_fd'):
